dea95fdbfe2c9b9bab41decded838839b0a30f4a
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2023-12-30 14:48:25 +1100

    remove unused import add '---' if not tl/inst in member list

--- tkdmanager/dashboard/views.py
+++ tkdmanager/dashboard/views.py
@@ -1,4 +1,3 @@
-from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import Member, Award, AssessmentUnit, GradingResult, Class, Payment, GRADINGS
