a2aa58ee47e3a490b6a0901f89bb827708d0031f
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-02 22:01:09 +1100

    try and get dotenv to work

--- tkdmanager/tkdmanager/settings.py
+++ tkdmanager/tkdmanager/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "hdsay71298yd8uoayd98qywd")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("DEBUG") == "False":
@@ -85,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'DjangoTKDManager',
         'USER': 'postgres',
-        'PASSWORD': "DjangoDB",
+        'PASSWORD': os.environ["POSTGRES_PASSWORD_DJANGO"],
         'HOST': 'localhost',
         'PORT': '5432',
     }
