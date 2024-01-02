ef4d5fb73207a91d3243651af4f5f8223178b7c8
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-02 18:59:18 +1100

    use environment vars

--- tkdmanager/tkdmanager/settings.py
+++ tkdmanager/tkdmanager/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ns++*xao*x4kj5e9qf)5&r)mh0wrgjr&^ne&q989#pfk7-s3w'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-@ns++*xao*x4kj5e9qf)5&r)mh0wrgjr&^ne&q989#pfk7-s3w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
