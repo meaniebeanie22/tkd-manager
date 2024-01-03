ee2f132fcceceb0d4633a934fb36a4962fd04a03
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-03 16:18:26 +1100

    fix home context?

--- tkdmanager/dashboard/views.py
+++ tkdmanager/dashboard/views.py
@@ -35,7 +35,7 @@ def index(request):
         'num_members': num_members,
         'num_active_members': num_active_members,
         'belt_labels': ["None","White","Orange","Yellow","Blue","Red","CDB","Black"],
-        'belt_count': [Member.objects.filter(belt__exact='').count(),Member.objects.filter(belt__in=('0','1','2','3','4','5','6','7')).count(),Member.objects.filter(belt__in=('8','9','10','11','12','13','14','15')).count(),Member.objects.filter(belt__in=('16','17','18','19','20','21','22','23')).count(),Member.objects.filter(belt__in=('24','25','26','27','28','29','30','31')).count(),Member.objects.filter(belt__in=('32','33','34','35','36','37','38')).count(),Member.objects.filter(belt__in=('39','40','41','42','43','44','45','46')).count(),Member.objects.filter(belt__in=('47','48','49','50','51','52','53','54','55')).count()]
+        'belt_count': [Member.objects.filter(belt__exact='').count(),Member.objects.filter(belt__in=tuple(range(8))).count(),Member.objects.filter(belt__in=tuple(range(8,16))).count(),Member.objects.filter(belt__in=tuple(range(16,24))).count(),Member.objects.filter(belt__in=tuple(range(24,32))).count(),Member.objects.filter(belt__in=tuple(range(32,39))).count(),Member.objects.filter(belt__in=tuple(range(39,47))).count(),Member.objects.filter(belt__in=tuple(range(47,56))).count()]
     }
 
     return render(request, 'home.html', context=context)
