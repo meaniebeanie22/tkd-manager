40ede663433a4ba8281f58fd43575dc6a12e36af
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-03 14:05:31 +1100

    try kwargs???

--- tkdmanager/dashboard/forms.py
+++ tkdmanager/dashboard/forms.py
@@ -83,7 +83,7 @@ class GradingInviteForm(ModelForm):
             'payment': AddAnotherEditSelectedWidgetWrapper(
                 forms.Select,
                 reverse_lazy('add-payment'),
-                reverse_lazy('update-payment', pk=['__fk__']),
+                reverse_lazy('update-payment', kwargs={'pk':['__fk__']}),
             )
         }
 
