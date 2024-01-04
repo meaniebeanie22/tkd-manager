1a008e3bbb1d10ac5237605a3cdbb4e82253a507
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-04 22:04:51 +1100

    forgot app name

--- tkdmanager/api/urls.py
+++ tkdmanager/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
+app_name = "api"
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'gradingresults', views.GradingResultViewSet)
