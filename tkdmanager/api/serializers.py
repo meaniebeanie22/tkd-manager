fbb611fb0f97bc04a366403c628a566ef8d9b450
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-04 22:14:45 +1100

    AAAAAAAAAAAAARGH

--- tkdmanager/api/serializers.py
+++ tkdmanager/api/serializers.py
@@ -4,11 +4,15 @@ from dashboard.models import GradingResult, AssessmentUnit, GradingInvite, Payme
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    extra_kwargs = {'url': {'view_name': 'api:user-detail'}}
+
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
 class AssessmentUnitSerializer(serializers.HyperlinkedModelSerializer):
+    extra_kwargs = {'url': {'view_name': 'api:assessmentunit-detail'}}
+
     class Meta:
         model = AssessmentUnit
         fields = ['unit','achieved_pts','max_pts','grading_result']
@@ -16,27 +20,36 @@ class AssessmentUnitSerializer(serializers.HyperlinkedModelSerializer):
 
 class GradingResultSerializer(serializers.HyperlinkedModelSerializer):
     assessmentunits = AssessmentUnitSerializer(many=True, read_only=True)
+    extra_kwargs = {'url': {'view_name': 'api:gradingresult-detail'}}
 
     class Meta:
         model = GradingResult
         fields = ['member','grading','forbelt','assessor','comments','award', 'assessmentunits','is_letter','gradinginvite']
 
 class GradingInviteSerializer(serializers.HyperlinkedModelSerializer):
+    extra_kwargs = {'url': {'view_name': 'api:gradinginvite-detail'}}
+
     class Meta:
         model = GradingInvite
         fields = ['member', 'forbelt', 'issued_by', 'payment', 'grading']
 
 class PaymentSerializer(serializers.HyperlinkedModelSerializer):
+    extra_kwargs = {'url': {'view_name': 'api:payment-detail'}}
+
     class Meta:
         model = Payment
         fields = ['member','paymenttype','date_created','date_due','date_paid_in_full','amount_due','amount_paid']
     
 class GradingSerializer(serializers.HyperlinkedModelSerializer):
+    extra_kwargs = {'url': {'view_name': 'api:grading-detail'}}
+
     class Meta:
         model = Grading
         fields = ['grading_type','grading_datetime']
 
 class PaymentTypeSerialzer(serializers.HyperlinkedModelSerializer):
+    extra_kwargs = {'url': {'view_name': 'api:paymenttype-detail'}}
+
     class Meta:
         model = PaymentType
         fields = ['name','standard_amount']
