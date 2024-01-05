1451284b369f00bcc367a3a2a1cc91506e6016f0
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-05 12:46:50 +1100

    fix?

--- tkdmanager/api/urls.py
+++ tkdmanager/api/urls.py
@@ -2,8 +2,6 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
-app_name = "api"
-
 router = routers.DefaultRouter()
 router.register(r'user', views.UserViewSet)
 router.register(r'gradingresult', views.GradingResultViewSet)
@@ -14,6 +12,6 @@ router.register(r'paymenttype', views.PaymentTypeViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls, 'api')),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
