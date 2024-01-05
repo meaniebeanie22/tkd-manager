6421cef958260699753f652facc8abc3dc5c57cf
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-05 12:41:24 +1100

    idk anymore

--- tkdmanager/api/urls.py
+++ tkdmanager/api/urls.py
@@ -14,6 +14,6 @@ router.register(r'paymenttype', views.PaymentTypeViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls, 'api')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
