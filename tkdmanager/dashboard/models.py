d84e115453cd67843b281bcdc17ee980960424da
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-30 19:17:03 +1100

    add code for recurringpayment model for testing

--- tkdmanager/dashboard/models.py
+++ tkdmanager/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 from django.utils import timezone
 from datetime import datetime, timedelta
@@ -250,3 +251,14 @@ class PaymentType(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+    
+class RecurringPayment(models.Model):
+    member = models.ForeignKey(Member, help_text='Who needs to pay this?', on_delete=models.PROTECT)
+    payments = models.ManyToManyField(Payment, help_text='What payments are linked to this')
+    last_payment_date = models.DateField() # should be the same as the creation date of the most recent payment
+    interval = models.DurationField()
+    amount = models.DecimalField(max_digits=7, decimal_places=2, help_text='Amount to be paid, in $', default=0)
+    next_due = models.GeneratedField(db_persist=True, output_field=models.DateTimeField(), expression=(F('last_payment_date') + F('interval')))
+
+    def __str__(self):
+        return f'Recurring Payment for {self.member}, ${self.amount} per {self.interval}'
