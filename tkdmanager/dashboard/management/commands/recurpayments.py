1926f2464eec3801df6d285a4b68fb4e74f61bb7
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-30 21:32:38 +1100

    add a success message to the cmd

--- tkdmanager/dashboard/management/commands/recurpayments.py
+++ tkdmanager/dashboard/management/commands/recurpayments.py
@@ -4,9 +4,15 @@ import datetime
 
 class Command(BaseCommand):
     def handle(self):
+        affected = 0
         for rpayment in RecurringPayment.objects.filter(next_due__lte=datetime.datetime.now()):
             # payments that need renewal
             p = rpayment.payments.create(member=rpayment.member, paymenttype=rpayment.paymenttype, amount_due=rpayment.amount)
             rpayment.last_payment_date = datetime.date.today()
             rpayment.save()
+            affected += 1
+
+        self.stdout.write(
+            self.style.SUCCESS(f'Successfully Recurred Payments.\nAffected {affected} RecurringPayments.')
+        )
 
