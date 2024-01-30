c8c99fe587a9d17745d6e887c52c411d474ba5dc
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-01-30 11:04:28 +1100

    use weasyprint to make pdfs normally

--- tkdmanager/dashboard/renderers.py
+++ tkdmanager/dashboard/renderers.py
@@ -1,8 +1,10 @@
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
-
+from requests import session, get, post
 from xhtml2pdf import pisa
+from sys import stderr
+import os
 
 def PDFResponse(template_src, filename, context_dict={}):
     result = render_to_pdf(template_src, context_dict)
@@ -21,7 +23,7 @@ def render_to_pdf(template_src, context_dict={}):
     return result
 """
 
-def render_to_pdf(template_src, context_dict):
+def base_render_to_pdf(template_src, context_dict):
     template = get_template(template_src)
     html  = template.render(context_dict)
     result = BytesIO()
@@ -29,3 +31,15 @@ def render_to_pdf(template_src, context_dict):
     if pisa_status.err:
        return None
     return result
+
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+
+    s = session()
+    health = s.get(os.environ.get('WEASYPRINT_API_DOMAIN') + '/api/v1.0/health')
+    if not(health):
+        print('Weasyprint API did not return HEALTH, using xhtml builtin instead.', file=stderr)
+        return base_render_to_pdf(template_src, context_dict)
+    s.headers['X_API_KEY'] = os.environ.get('WEASYPRINT_API_KEY')
+    return s.post(os.environ.get('WEASYPRINT_API_DOMAIN') + '/api/v1.0/print', {'html': html})
