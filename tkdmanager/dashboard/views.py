c358acbcee1ba332de9bd6ced2e019148c6e8f68
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-14 17:03:48 +1100

    more changes

--- tkdmanager/dashboard/views.py
+++ tkdmanager/dashboard/views.py
@@ -89,9 +89,11 @@ class MemberListView(LoginRequiredMixin, generic.ListView):
 
             # Iterate over form fields and add filters dynamically
             for field_name, value in form.cleaned_data.items():
-                print(f'Field, Value: {field_name} {value}')
+                print(f'Field, Value: {field_name} || {value}')
                 if field_name == 'member':
-                    return value
+                    queryset = QuerySet(value)
+                    print(f'Queryset: {queryset}')
+                    return queryset
                 if value:
                     filters[field_name] = value
             
