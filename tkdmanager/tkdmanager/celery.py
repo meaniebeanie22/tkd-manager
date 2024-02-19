00084ebb85cc012a78e62eb1d552310b2a10a62e
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-20 10:13:37 +1100

    update how celery looks for apps

--- tkdmanager/tkdmanager/celery.py
+++ tkdmanager/tkdmanager/celery.py
@@ -7,8 +7,9 @@ app = Celery("tkdmanager")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
-app.autodiscover_tasks()
+from django.apps import apps 
 
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
 @app.task(bind=True, ignore_result=True)
 def example_task(self):
