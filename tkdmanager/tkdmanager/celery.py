39dcd5e432364712f9a1ac77494ea3bb45646582
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-21 18:25:06 +1100

    add a clearsessions task to celery

--- tkdmanager/tkdmanager/celery.py
+++ tkdmanager/tkdmanager/celery.py
@@ -14,3 +14,11 @@ app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 @app.task(bind=True, ignore_result=True)
 def example_task(self):
     print("You've triggered the example task!")
+
+from django.core import management
+
+@app.task(bind=True)
+def clearsessions(self):
+    print('Clearing sessions.')
+    management.call_command('clearsessions')
+    print('Sessions cleared.')
