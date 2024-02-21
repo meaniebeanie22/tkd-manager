edf7a22b1d4a08b4490a2412ff90389b58d086f1
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-21 14:43:59 +1100

    magic

--- tkdmanager/dashboard/forms.py
+++ tkdmanager/dashboard/forms.py
@@ -466,6 +466,12 @@ class BeltForm(ModelForm):
         fields = ['name']
 
 class StyleForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
+            
     class Meta:
         model = Style
         fields = ['name']
