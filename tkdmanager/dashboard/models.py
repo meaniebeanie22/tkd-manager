6afa875fde424153fd98c3e4da9c7528c473880d
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-23 19:52:01 +1100

    print for debugging

--- tkdmanager/dashboard/models.py
+++ tkdmanager/dashboard/models.py
@@ -180,6 +180,7 @@ class Member(models.Model):
         """
         Use the context dict from a view to extract the style
         """
+        print(f'Context: {dir(context)}')
         style = context.session.get('style')
         return self.get_belt(get_object_or_404(Style, pk=style))
 
