ed75d1ca96ce01e726390039ebc5322535914b53
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-23 15:04:16 +1100

    change the names of the tags to remove the underscores?

--- tkdmanager/dashboard/templatetags/dashboard_extras.py
+++ tkdmanager/dashboard/templatetags/dashboard_extras.py
@@ -9,15 +9,11 @@ def has_group(user, group_name):
     return True if group in user.groups.all() else False
 
 @register.simple_tag
-def call_method(obj, method_name, *args):
+def callmethod(obj, method_name, *args):
     method = getattr(obj, method_name)
     return method(*args)
 
 @register.simple_tag(takes_context=True)
-def call_method_context(context, obj, method_name, *args):
+def callmethodcontext(context, obj, method_name, *args):
     method = getattr(obj, method_name)
     return method(context, *args)
-
-@register.simple_tag
-def print_potato():
-    return 'potatos are very cool'
