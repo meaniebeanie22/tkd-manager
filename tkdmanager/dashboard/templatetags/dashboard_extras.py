cf5340aa0e97bf478f2ff759ac8756e48d914f99
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-23 15:05:03 +1100

    add name args to decorators

--- tkdmanager/dashboard/templatetags/dashboard_extras.py
+++ tkdmanager/dashboard/templatetags/dashboard_extras.py
@@ -8,12 +8,12 @@ def has_group(user, group_name):
     group = Group.objects.get(name=group_name) 
     return True if group in user.groups.all() else False
 
-@register.simple_tag
+@register.simple_tag(name='callmethod')
 def callmethod(obj, method_name, *args):
     method = getattr(obj, method_name)
     return method(*args)
 
-@register.simple_tag(takes_context=True)
+@register.simple_tag(takes_context=True, name='callmethodcontext')
 def callmethodcontext(context, obj, method_name, *args):
     method = getattr(obj, method_name)
     return method(context, *args)
