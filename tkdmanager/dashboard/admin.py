53feb1e7ca43563526d876f7d54ca93ae9c99068
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-02-25 20:28:48 +1100

    test

--- tkdmanager/dashboard/admin.py
+++ tkdmanager/dashboard/admin.py
@@ -28,4 +28,4 @@ from django.contrib.admin.views.decorators import staff_member_required
 # admin.site.login = staff_member_required(admin.site.login, login_url=settings.LOGIN_URL)
 
 
-admin.site.login = staff_mfa_required(admin.site.login, login_url=settings.LOGIN_URL)
+admin.site.login = staff_mfa_required(admin.site.login)
