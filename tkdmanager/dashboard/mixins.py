ba81667e549f254623400760187d2380fade1d5e
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-03-01 20:40:25 +1100

    add a print to the new one

--- tkdmanager/dashboard/mixins.py
+++ tkdmanager/dashboard/mixins.py
@@ -11,11 +11,13 @@ def user_is_mfa(userobj):
             return False
         # get user session
         session = Session.objects.filter(_auth_user_id=userobj.pk).first()
+        print(f'Session: {session.items()}')
         for method in session['account_authentication_methods']:
             if method['method'] == 'mfa':
                 return True
             else:
                 return False
+            
     except Exception as error:
         print(f"Error in user_is_mfa: {error}")
         return False
