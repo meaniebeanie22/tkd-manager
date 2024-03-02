31994515adf3087ddf7ccaff16de2f0b88154249
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2024-03-02 19:10:29 +1100

    bugfix

--- tkdmanager/dashboard/mixins.py
+++ tkdmanager/dashboard/mixins.py
@@ -3,9 +3,10 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .decorators import req_user_is_mfa
 
+
 class MFARequiredMixin(UserPassesTestMixin):
     def test_func(self):
-        return req_user_is_mfa(self.request.user)
+        return req_user_is_mfa(self.request)
 
     def handle_no_permission(self) -> HttpResponseRedirect:
         return HttpResponseRedirect(reverse("mfa_activate_totp"))
